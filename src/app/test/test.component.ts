67387b62b38d890564a71e4f5cd15c6e1849b3a5
Idan Cohen <idan@idancohen.com>
2020-04-04 20:11:26 +0300

    basic openseadragon with local assets

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -1,4 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import * as OpenSeadragon from 'openseadragon';
+import { TiledImageOptions } from 'openseadragon';
+
+const OSD_OPTIONS: OpenSeadragon.Options = {
+  // preload: true,
+  showNavigator: false, // image map
+  showNavigationControl: false, // zoom, etc.
+  // maxZoomPixelRatio: 3,
+  // minZoomImageRatio: 1,
+  // visibilityRatio: 1, // prevent moving image outside viewport
+  // crossOriginPolicy: 'Anonymous', // required for openseadragon-filtering
+  // gestureSettingsMouse: {
+  //   clickToZoom: false,
+  //   dblClickToZoom: false,
+  // },
+  // preserveImageSizeOnResize: false
+};
 
 @Component({
   selector: 'app-test',
@@ -6,10 +23,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./test.component.scss']
 })
 export class TestComponent implements OnInit {
+  viewer: OpenSeadragon.Viewer;
+  @ViewChild('viewer', { static: true }) viewerElement: ElementRef;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.viewer = new OpenSeadragon.Viewer({
+      element: this.viewerElement.nativeElement,
+      ...OSD_OPTIONS
+    });
+
+    const tiledImageOptions: TiledImageOptions = {
+      tileSource: '/assets/test/test.dzi',
+    };
+
+    this.viewer.open(tiledImageOptions);
   }
 
 }
