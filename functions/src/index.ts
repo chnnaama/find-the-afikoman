0420a70631ea5f0088dd8ab92f289de591fe1f7d
Idan Cohen <idan@idancohen.com>
2020-04-04 12:04:22 +0300

    basic dzi processing

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -4,6 +4,7 @@ const gcs = new storage.Storage();
 import { tmpdir } from 'os';
 import { join, dirname } from 'path';
 
+// import * as globby from 'globby';
 import * as sharp from 'sharp';
 import * as fs from 'fs-extra';
 
@@ -25,7 +26,7 @@ export const generateThumbs = functions.storage
     const workingDir = join(tmpdir(), 'thumbs');
     const tmpFilePath = join(workingDir, 'source.png');
 
-    if (fileName!.includes('thumb@') || !object.contentType!.includes('image')) {
+    if (fileName!.includes('thumb@') || fileName!.includes('dzi@') || filePath!.includes('dzi@') || !object.contentType!.includes('image')) {
       console.log('exiting function');
       return false;
     }
@@ -39,7 +40,7 @@ export const generateThumbs = functions.storage
     });
 
     // 3. Resize the images and define an array of upload promises
-    const sizes = [64, 128, 256];
+    const sizes = [256];
 
     const uploadPromises = sizes.map(async size => {
       const thumbName = `thumb@${size}_${fileName}`;
@@ -58,6 +59,87 @@ export const generateThumbs = functions.storage
 
     // 4. Run the upload operations
     await Promise.all(uploadPromises);
+
+    // 5. Cleanup remove the tmp/thumbs from the filesystem
+    return fs.remove(workingDir);
+  });
+
+export const generateDZI = functions.storage
+  .object()
+  .onFinalize(async object => {
+    const bucket = gcs.bucket(object.bucket);
+    const filePath = object.name;
+    const fileName = filePath?.split('/').pop();
+    const bucketDir = dirname(filePath!);
+
+    const workingDir = join(tmpdir(), 'dzis');
+    const tmpFilePath = join(workingDir, 'source.png');
+
+    const tilesList: string[] = [];
+
+    if (fileName!.includes('thumb@') || fileName!.includes('dzi@') || filePath!.includes('dzi@') || !object.contentType!.includes('image')) {
+      console.log('exiting function');
+      return false;
+    }
+
+    // 1. Ensure thumbnail dir exists
+    await fs.ensureDir(workingDir);
+
+    // 2. Download Source File
+    await bucket.file(filePath!).download({
+      destination: tmpFilePath
+    });
+
+    console.info('downloaded');
+
+    const dziName = `dzi@${fileName}`;
+    const dziPath = join(workingDir, dziName);
+
+    // Resize source image
+    await sharp(tmpFilePath)
+      .jpeg()
+      .tile({
+        size: 1024
+      })
+      .toFile(dziPath);
+
+    console.info('sharped');
+
+    async function getFiles(directory: string) {
+      const items = await fs.readdir(directory);
+      for (const item of items) {
+        const fullPath = join(directory, item);
+        const stat = await fs.stat(fullPath);
+        if (stat.isFile()) {
+          tilesList.push(fullPath);
+        } else if (stat.isDirectory()) {
+          await getFiles(fullPath);
+        }
+      }
+    }
+
+    await getFiles(dziPath + '_files');
+
+    console.info(tilesList[0], tilesList[10]);
+
+    // const tiles = await globby(`${dziPath + '_files'}/**/*`);
+    // console.info({ tiles });
+
+    // Upload to GCS
+    await bucket.upload(dziPath + '.dzi', {
+      destination: join(bucketDir, dziName + '.dzi')
+    });
+
+    for (const tile of tilesList) {
+      const tileRelativePath = tile.split("_files/").pop();
+      const tileDirectory = tileRelativePath!.split('/')[0];
+      const tileFileName = tileRelativePath!.split('/')[1];
+      await bucket.upload(tile, {
+        destination: join(bucketDir + '/' + dziName + '_files/'+ tileDirectory, tileFileName)
+      });
+    }
+
+    console.info('uploaded', dziPath + '.dzi', bucketDir, dziName + '.dzi');
 
     // 5. Cleanup remove the tmp/thumbs from the filesystem
     return fs.remove(workingDir);
