dfa55d57ef41cc2128f13ec6a04d82ced595df8c
Idan Cohen <idan@idancohen.com>
2020-04-05 17:20:56 +0300

    add auth guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,17 +2,24 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { TestComponent } from './test/test.component';
 import { ListComponent } from './list/list.component';
+import { AuthGuard } from './auth.guard';
 
 
 const routes: Routes = [
   {
     path: '',
-    pathMatch: 'full',
-    component: ListComponent
-  },
-  {
-    path: 'test',
-    component: TestComponent
+    canActivate: [AuthGuard],
+    children: [
+      {
+        path: '',
+        pathMatch: 'full',
+        component: ListComponent
+      },
+      {
+        path: 'test',
+        component: TestComponent
+      }
+    ]
   }
 ];
 
---
+++ src/app/auth.guard.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthService } from './auth.service';
+import { map, take, tap } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+
+  constructor(private auth: AuthService) {}
+
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    return this.auth.user$.pipe(
+      take(1),
+      tap(() => {}),
+      map(user => !!user)
+    );
+  }
+
+}
