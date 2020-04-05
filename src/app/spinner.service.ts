27b08bc46feaa681fe58b7fea3be182565671990
Idan Cohen <idan@idancohen.com>
2020-04-05 21:46:11 +0300

    improve spinner

--- src/app/spinner.service.ts
+++ src/app/spinner.service.ts
@@ -5,14 +5,20 @@ import { BehaviorSubject } from 'rxjs';
   providedIn: 'root'
 })
 export class SpinnerService {
-  enabled$: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
+  enabled$: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(true);
+  text$: BehaviorSubject<string> = new BehaviorSubject<string>('');
 
   constructor() { }
 
-  toggle(value?) {
+  toggle(value?: boolean) {
+    this.setText('');
     if (value === undefined) {
       value = !this.enabled$.getValue();
     }
     this.enabled$.next(value);
+  }
+
+  setText(value: string) {
+    this.text$.next(value);
   }
 }
