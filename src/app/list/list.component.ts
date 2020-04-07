aaacf86f679f908018727f6eabcb80d73b6dd085
Idan Cohen <idan@idancohen.com>
2020-04-07 03:00:45 +0300

    add custom challenges to the top

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -3,6 +3,7 @@ import { Router } from '@angular/router';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
 import { Challenge } from '../types/challenge';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-list',
@@ -11,16 +12,23 @@ import { Challenge } from '../types/challenge';
 })
 export class ListComponent implements OnInit {
   challenges$: Observable<Challenge[]>;
+  privateChallenges$: Observable<Challenge[]>;
 
   constructor(private router: Router,
+              private auth: AuthService,
               private db: AngularFirestore) { }
 
   ngOnInit(): void {
-    const colRef = this.db.collection<Challenge>('challenges', ref => ref
+    const colRef1 = this.db.collection<Challenge>('challenges', ref => ref
       .where('public', '==', true)
       .orderBy('level')
     );
-    this.challenges$ = colRef.valueChanges();
+    const colRef2 = this.db.collection<Challenge>('challenges', ref => ref
+      .where('uid', '==', this.auth.user.uid)
+      .where('public', '==', false)
+    );
+    this.challenges$ = colRef1.valueChanges();
+    this.privateChallenges$ = colRef2.valueChanges();
   }
 
   openChallenge(challenge: Challenge) {
