8d5667eacbf5dee1cb1f0311cf75337ab404aaad
Idan Cohen <idan@idancohen.com>
2020-04-08 18:03:36 +0300

    add analytics per page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
 import { AuthService } from './auth.service';
 import { SpinnerService } from './spinner.service';
+import { NavigationEnd, Router } from '@angular/router';
+
+// tslint:disable-next-line:ban-types
+declare let gtag: Function;
 
 @Component({
   selector: 'app-root',
@@ -10,6 +14,18 @@ import { SpinnerService } from './spinner.service';
 export class AppComponent {
 
   constructor(public auth: AuthService,
-              public spinnerService: SpinnerService) { }
+              public router: Router,
+              public spinnerService: SpinnerService) {
+    this.router.events.subscribe(event => {
+        if (event instanceof NavigationEnd){
+          gtag('config', 'UA-162696534-2',
+            {
+              page_path: event.urlAfterRedirects
+            }
+          );
+        }
+      }
+    );
+  }
 
 }
